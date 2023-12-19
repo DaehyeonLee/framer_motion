7d3e21960e5d7123ea61ba2c1e96c81f26e5bd07
Daehyeon Lee <31493850+DaehyeonLee@users.noreply.github.com>
2023-12-19 14:51:12 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,91 @@
 import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(135deg, #e09, #d0e);
+`;
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 25, 0.2);
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  background-color: white;
+  border-radius: 35px;
+  box-shadow: 0 2px 30x rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+// const myVars = {
+//   start: { scale: 0 },
+//   end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 1, bounce: 0.5 } },
+// };
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const CircleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 function App() {
-  return <div>1111</div>;
+  return (
+    <Wrapper>
+      <Box
+        variants={boxVariants}
+        initial="start"
+        animate="end"
+        //variant를 이용 기초
+        // variants={myVars}
+        // initial="start"
+        // animate="end"
+
+        //기본적인 방법
+        // transition={{ type: "spring", delay: 1, bounce: 0.5 }}
+        // initial={{ scale: 0 }}
+        // animate={{ scale: 1, rotateZ: 360 }}
+      >
+        <Circle variants={CircleVariants} />
+        <Circle variants={CircleVariants} />
+        <Circle variants={CircleVariants} />
+        <Circle variants={CircleVariants} />
+      </Box>
+    </Wrapper>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { RecoilRoot } from "recoil";
+import "reset-css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
